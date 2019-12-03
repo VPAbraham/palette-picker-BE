dda2856c32d69056ce137a15f108fd6280844b1d
VPAbraham <vpa456@gmail.com>
2019-12-03 15:19:48 -0700

    Add sedding data and establish relationship between palette and project tables

--- db/seeds/dev/seeds.js
+++ db/seeds/dev/seeds.js
@@ -1,18 +1,29 @@
+const projectsData = require('../../../sample_data/projectsData');
+const palettesData = require('../../../sample_data/palettesData');
 
-exports.seed = function(knex) {
-  // Deletes ALL existing entries
-  return knex('palettes').del()
-  .then(() => knex('projects').del())
-
-  .then(() => {
-    return Promise.all([
-
-      knex('projects').insert({
-        name: 'Neature'
-      }, 'id')
-    ])
+const createProject = (knex, project) => {
+  return knex('projects').insert({
+    'name': project.name
+  }, 'id')
+  .then(projectId => {
+    let palettesPromises =[];
+    palettesData.filter(palette => palette.projects_id === projectId[0])
+    .forEach(palette => {
+      palettesPromises.push(
+        createPalette(knex, {
+          name: palette.name,
+          color1: palette.color1,
+          color2: palette.color2,
+          color3: palette.color3,
+          color4: palette.color4,
+          color5: palette.color5,
+          projects_id: projectId[0]
+        })
+      )
+    });
+    return Promise.all(palettesPromises);
   })
-};
+}
 
 const createPalette = (knex, palette) => {
   return knex('palettes').insert(palette)
@@ -23,7 +34,7 @@ exports.seed = (knex) => {
   .then(() => knex('projects').del())
   .then(() => {
     let projectPromises = [];
-    projectData.forEach(project => {
+    projectsData.forEach(project => {
       projectPromises.push(createProject(knex, project))
     });
     return Promise.all(projectPromises);
