17de371524d81e5a1a094c4cccdc20a7edfec902
Quinne Farenwald <q.farenwald@gmail.com>
2019-12-04 14:48:47 -0700

    Edits to test installs to get test suite running

--- app.test.js
+++ app.test.js
@@ -1,11 +1,17 @@
-import '@babel/polyfill';
-import request from 'supertest';
-import app from './app';
-
-const environment = process.env.NODE_ENV || 'development';
-const configuration = require('./knexfile')[environment];
-const database = require('knex')(configuration);
+import "@babel/polyfill";
+import request from 'supertest'
+import app from './app'
+const environment = process.env.NODE_ENV || 'development'
+const configuration = require('./knexfile')[environment]
+const database = require('knex')(configuration)
 
 describe('Server', () => {
+
+  describe('init', () => {
+    it('should return a 200 status', async () => {
+      const res = await request(app).get('/')
+      expect(res.status).toBe(200)
+    });
+  });
   
-})
+});
