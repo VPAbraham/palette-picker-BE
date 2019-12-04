5418de88b4bc8cda74275c23bc86b493afa678cf
VPAbraham <vpa456@gmail.com>
2019-12-04 14:09:15 -0700

    FIll out app testing file boilerplater, install babel polyfill, install supertest

---
+++ app.test.js
@@ -0,0 +1,11 @@
+import '@babel/polyfill';
+import request from 'supertest';
+import app from './app';
+
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('./knexfile')[environment];
+const database = require('knex')(configuration);
+
+describe('Server', () => {
+  
+})
