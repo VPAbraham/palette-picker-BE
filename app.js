b7d65ce5e1a05b1ee2aaa7285ee0d424304d43b0
Quinne Farenwald <q.farenwald@gmail.com>
2019-12-04 18:39:56 -0700

    Write POST a palette functionality

--- app.js
+++ app.js
@@ -63,7 +63,24 @@ app.get('/api/v1/palettes/:id', async (request, response) => {
 })
 
 //POST
+app.post('/api/v1/palettes', (request, response) => {
+  const palette = request.body;
 
+  for (let requiredParameter of ['name', 'color1', 'color2', 'color3', 'color4', 'color5']) {
+    if (!palette[requiredParameter]) {
+      return response.status(422).send({
+        error: `Expected format: { name: <String>, color1: <String>, color2: <String>, color3: <String>, color4: <String>, color5: <String> }. You're missing a "${requiredParameter}" property.`
+      })
+    }
+  }
+  database('palettes').insert(palette, 'id')
+    .then(palette => {
+      response.status(201).json({ id: palette[0] })
+    })
+    .catch(error => {
+      response.status(500).json({ error })
+    })
+})
 
 //PATCH
 
