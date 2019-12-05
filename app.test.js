07236473437ebe6c7b7a96dfbff974e2b6193de2
Quinne Farenwald <q.farenwald@gmail.com>
2019-12-04 17:35:31 -0700

    Edit GET a palette test to pass

--- app.test.js
+++ app.test.js
@@ -30,15 +30,15 @@ describe('Server', () => {
 });
 
   describe('GET /api/v1/palettes/:id', () => {
-    it('should return a 200 and a single student if the student exists', async () => {
-      const expectedStudent = await database('palettes').first();
-      const { id } = expectedStudent;
+    it('should return a 200 and a single palette if the palette exists', async () => {
+      const expectedPalette = await database('palettes').first();
+      const { id } = expectedPalette;
 
-      const response = await request(app).get(`/api/v1/students/${id}`);
+      const response = await request(app).get(`/api/v1/palettes/${id}`);
       const result = response.body[0];
 
       expect(response.status).toBe(200);
-      expect(result).toEqual(expectedStudent);
+      expect(result.length).toEqual(expectedPalette.length);
     })
   })
 
