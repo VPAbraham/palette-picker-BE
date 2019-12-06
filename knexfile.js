b525abb4612d833d24858cbf94da86008f53cff5
VPAbraham <vpa456@gmail.com>
2019-12-06 13:18:08 -0700

    Fix seeds url in knexfile for prod

--- knexfile.js
+++ knexfile.js
@@ -32,7 +32,7 @@ module.exports = {
       directory: './db/migrations'
     },
     seeds: {
-      directory: '.db/seeds/dev/seeds'
+      directory: './db/seeds/dev'
     },
     useNullAsDefault: true
   }
